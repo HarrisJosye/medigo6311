5fe89d50c19df920827022b84337b130502d9426
MandeepMandla <78023084+MandeepMandla@users.noreply.github.com>
2021-04-08 07:16:28 -0400

    MG-11 working signup & login page w db(todo:Java)

--- app.py
+++ app.py
@@ -1,31 +1,16 @@
-from flask import (Flask, redirect, render_template, request, session, url_for)
-
-
-class client:
-    def __init__(self, id_no, username, password):
-        self.id = id_no
-        self.username = username
-        self.password = password
-
-    def __repr__(self):
-        return f'<User : {self.username}>'
-
-
-clients = []
-clients.append(client(id_no=1, username='admin', password='admin'))
-clients.append(client(id_no=2, username='mandeepmandla97@gmail.com', password='mandeep'))
-
-# class registration:
-#     def __init__(self, id_no, username, password, email, contact):
-#         self.id = id_no
-#         self.username = username
-#         self.password = password
-#         self.email = email
-#         self.contact = contact
-
+from flask import (Flask, flash, redirect, render_template, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from multiprocessing import Process
+import time
 
 app = Flask(__name__)
 app.secret_key = 'idontknowitsroleyetbutneedit'
+app.config[
+    'MONGO_URI'] = 'mongodb+srv://mandeepmandla:IAb8tM0lYpp9j5xJ@cluster0.vspam.mongodb.net/MediGO?retryWrites=true&w=majority'
+mongo = PyMongo(app)
+LoginDB = mongo.db.Login
+SignupDB = mongo.db.SignUp
 
 
 @app.route('/')
@@ -33,19 +18,20 @@ def welcome_page():
     return render_template('welcome_page.html')
 
 
-@app.route('/login_page', methods=['GET', 'POST'])
+@app.route('/login_page', methods=['POST', 'GET'])
 def login_page():
     if request.method == 'POST':
-        session.pop('client_id', None)
-        username = request.form['username']
-        password = request.form['password']
 
-        user_details = [x for x in clients if x.username == username][0]
-        if user_details and user_details.password == password:
-            session['client_id'] = user_details.id
-            session['username'] = user_details.username
-            return redirect(url_for('welcome_page'))
+        get_user = LoginDB.find_one({'Username': request.form['username']})
+        get_password = LoginDB.find_one({'Password': request.form['password']})
+        get_email = LoginDB.find_one({'Email': request.form['username']})
+
+        if get_user or get_email:
+            if get_password:
+                print("Login successful")
+                return redirect(url_for('welcome_page'))
         else:
+            print("Username or password incorrect.")
             return redirect(url_for('login_page'))
 
     return render_template('login_page.html')
@@ -53,19 +39,36 @@ def login_page():
 
 @app.route('/signup_page', methods=['GET', 'POST'])
 def signup_page():
-    # error = ''
-    # if "username" in session:
-    #     return redirect(url_for('login_page'))
-    # if request.method == 'POST':
-    #     user = request.form.get("username")
-    #     email = request.form.get("Email-id")
-    #
-    #     password_1 = request.form.get("Password")
-    #     password_2 = request.form.get("Confirm-Password")
-    #
-    #     category = request.form.get("Category")
-    return render_template('signup_page.html')
+    userlist = [{'type': 'New user'}, {'type': 'New NGO'}]
+    if "username" in session:
+        return redirect(url_for('login_page'))
+    if request.method == 'POST':
+        password_1 = request.form.get("password")
+        password_2 = request.form.get("confirmpassword")
+
+        category = request.form.get('category')
+
+        user_exist = LoginDB.find_one({'Username': request.form['username']})
+        email_exist = LoginDB.find_one({'email': request.form['email']})
+
+        if user_exist or email_exist:
+            print("User already exists")
+            return redirect(url_for('login_page'))
+        if password_1 != password_2:
+            print("Password and confirm password doesn't match")
+            return redirect(url_for('signup_page'))
+        else:
+            new_user = ({'Username': request.form['username'], 'Email': request.form['email'], 'Password': password_1,
+                         'Category': category})
+            new_user_login = (
+                {'Username': request.form['username'], 'Email': request.form['email'], 'Password': password_1})
+            SignupDB.insert(new_user)
+            LoginDB.insert(new_user_login)
+
+            return redirect(url_for('welcome_page'))
+
+    return render_template('signup_page.html', userlist=userlist)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', debug=True)
